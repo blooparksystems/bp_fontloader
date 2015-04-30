fba337319a36e2afa26171b5c2f6cd4b96766275
Benjamin Bachmann <bbachmann@bloopark.de>
2015-04-30 11:48:58 +0200

    [REF] checked for PEP8 and PEP257 Code conventions and added License

--- models/ir_ui_view.py
+++ models/ir_ui_view.py
@@ -1,16 +1,36 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Odoo, an open source suite of business apps
+# This module copyright (C) 2015 bloopark systems (<http://bloopark.de>).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 from openerp.models import Model
 
 
 class View(Model):
+
+    """View class."""
+
     _inherit = 'ir.ui.view'
 
-    def render(self, cr, uid, id_or_xml_id,
-               values=None, engine='ir.qweb',
-               context=None):
+    def render(self, cr, uid, id_or_xml_id, values=None, engine='ir.qweb', context=None):
         """
-        Override of the render function to make sure
-        the website object is available in each template.
+        Override of the render function to make sure the website object is available in each
+        template.
 
         :param cr: object
         :param uid: integer
@@ -25,7 +45,7 @@ class View(Model):
         # if no website object is provided it will
         # be added to the values dictionary to ensure the availability
         # of the current website object in all templates
-        if not 'website' in values:
+        if 'website' not in values:
             w_obj = self.pool.get('website')
             if w_obj:
                 current_website = w_obj.get_current_website(
